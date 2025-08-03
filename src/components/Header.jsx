227ee60440f27e6d7f4610f692d97e802ace83d6
srinu0906 <164818954+srinu0906@users.noreply.github.com>
2025-08-03 23:33:56 +0530

    responsive nav bar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,36 @@
 import { useState } from "react";
 
-function handleToggle() {
-  const nav = document.querySelector("header nav");
-  const toggle = document.getElementById("toggle");
-  toggle.innerText = toggle.innerText === "☰" ? "✖" : "☰";
-  nav.classList.toggle("active");
-  // Toggle the active class on the nav element
-}
+// function handleToggle() {
+//   const nav = document.querySelector("header nav");
+//   const toggle = document.getElementById("toggle");
+//   toggle.innerText = toggle.innerText === "☰" ? "✖" : "☰";
+//   nav.classList.toggle("active");
+//   // Toggle the active class on the nav element
+// }
 
-const [toggle, setToggle] = useState("toggle");
+// function handleToggle(){
+//   // const toggle = document.getElementById("togggle");
+//   // toggle.innerText = toggle.innerText == "☰" ? "✖" : "☰";
+//   document.getElementById("togggle").nodeValue = "X"
+// }
+
+// const [toggle, setToggle] = useState("toggle");
 
 function Header() {
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggle = () => {
+    setIsOpen(current => !current);
+    // const toggle = document.getElementById("toggle");
+    // toggle.classList.
+  }
+
   return (
     <header>
         <h1>Srinu Vakada</h1>
-        <div className="toggle" id="toggle" >☰</div>
-        <nav>
+        <div className={isOpen?"toggle cross":"toggle bar"} id="toggle" onClick={()=>handleToggle()}>{isOpen ? "✖" : "☰"}</div>
+        <nav className={isOpen ? "show-nav" : "hide-nav"}>
             <a className="active" href="#home">Home</a>
             <a href="#about">About</a>
             <a href="#skills">Skills</a>
